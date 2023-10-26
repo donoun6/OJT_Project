19fa095b0e9bcb6c9ed36870a7ff10b636f30766
donoun6 <donoun6@naver.com>
2023-10-26 16:49:41 +0900

    admin main frame, side slide js

--- src/main/resources/static/js/admin/aside.js
+++ src/main/resources/static/js/admin/aside.js
@@ -6,7 +6,7 @@ $(function () {
         $("nav").stop().animate({ opacity : "0" },500,"easeInExpo");
         $(".arrow-back-ios").stop().animate({ opacity : "0" },250,"easeOutExpo");
         $(".arrow-forward-ios").stop().animate({ opacity : "1" , left : "-1.5%"},500,"easeInExpo");
-        $(".main-wrap").stop().animate({ marginLeft : "0" , width : "100%" },450,"easeInExpo");
+        $("#main-wrap").stop().animate({ marginLeft : "0" , width : "100%" },450,"easeInExpo");
     });
 
     $(".arrow-forward-ios").click(function () {
@@ -15,7 +15,7 @@ $(function () {
         $("nav").stop().animate({ opacity : "1" },500,"easeInExpo");
         $(".arrow-back-ios").stop().animate({ opacity : "1" },450,"easeInExpo");
         $(".arrow-forward-ios").stop().animate({ opacity : "0" , left : "-3%"},500,"easeInExpo");
-        $(".main-wrap").stop().animate({ marginLeft : "13%" , width : "87%" },500,"easeInExpo");
+        $("#main-wrap").stop().animate({ marginLeft : "13%" , width : "87%" },500,"easeInExpo");
     });
 
 });
