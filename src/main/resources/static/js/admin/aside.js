7c1ce64018b48cc3e99478af2a69cd15bdc24614
donoun6 <donoun6@naver.com>
2023-10-29 21:38:27 +0900

    product / css 추가

--- src/main/resources/static/js/admin/aside.js
+++ src/main/resources/static/js/admin/aside.js
@@ -1,5 +1,5 @@
 $(function () {
-    $("aside").load("common/aside.html")
+    $("aside").load("/templates/admin/common/aside.html")
 
     //aside slide
     $(".arrow-back-ios").click(function () {
@@ -8,7 +8,7 @@ $(function () {
         $("nav").stop().animate({ opacity : "0" },500,"easeInExpo");
         $(".arrow-back-ios").stop().animate({ opacity : "0" },250,"easeOutExpo");
         $(".arrow-forward-ios").stop().animate({ opacity : "1" , left : "-1.5%"},500,"easeInExpo");
-        $("#main-wrap").stop().animate({ marginLeft : "0" , width : "100%" },450,"easeInExpo");
+        $("#main-wrap").stop().animate({ marginLeft : "0" , width : "100%" },550,"easeInExpo");
     });
 
     $(".arrow-forward-ios").click(function () {
@@ -17,7 +17,7 @@ $(function () {
         $("nav").stop().animate({ opacity : "1" },500,"easeInExpo");
         $(".arrow-back-ios").stop().animate({ opacity : "1" },450,"easeInExpo");
         $(".arrow-forward-ios").stop().animate({ opacity : "0" , left : "-3%"},500,"easeInExpo");
-        $("#main-wrap").stop().animate({ marginLeft : "13%" , width : "87%" },500,"easeInExpo");
+        $("#main-wrap").stop().animate({ marginLeft : "13%" , width : "87%" },400,"easeInExpo");
     });
 
 });
