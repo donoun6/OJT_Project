c30ea288177507055ebb430c7bdbaa51a9c906a3
donoun6 <donoun6@naver.com>
2023-11-08 15:13:26 +0900

    add mouseover img preview event

--- src/main/resources/static/js/common.js
+++ src/main/resources/static/js/common.js
@@ -239,3 +239,38 @@ $(function () {
         prevText: "<"
     });
 });
+
+/**
+ * mouse over img preview
+ */
+$(document).ready(function() {
+    var xOffset = 210;
+    var yOffset = 10;
+
+    //마우스 오버시 preview 생성
+    $(document).on("mouseover",".gallery",function(e){
+        console.log($(this).attr("src"));
+        var image_data = $(this).data("image");
+        var add_caption = (image_data != undefined) ? "<br/>" + image_data : "" ;
+        $("body").append("<p id='preview' style='position: absolute; z-index: 9'>" +
+            "<img style='opacity: 0.9' src='"+ $(this).attr("src") +"' width='230px' />"+ add_caption +"</p>");
+        $("#preview")
+            .css("top",(e.pageY - xOffset) + "px")
+            .css("left",(e.pageX + yOffset) + "px")
+            .fadeIn("fast");
+    });
+
+    //마우스 이동시 preview 이동
+    $(document).on("mousemove",".gallery",function(e){
+        console.log("ddd")
+        $("#preview")
+            .css("top",(e.pageY - xOffset) + "px")
+            .css("left",(e.pageX + yOffset) + "px");
+    });
+
+    //마우스 아웃시 preview 제거
+    $(document).on("mouseout",".gallery",function(){
+        console.log("dddcxxx")
+        $("#preview").remove();
+    });
+});
