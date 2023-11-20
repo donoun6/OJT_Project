7af5ac268022e89d885a99b51ce8b19376767163
donoun6 <donoun6@naver.com>
2023-11-20 17:04:16 +0900

    데이터 초기화 파일 생성

--- src/main/resources/static/db/initial.sql
+++ src/main/resources/static/db/initial.sql
@@ -1,4 +1,88 @@
--- **임시 데이터**
+#초기화 데이터 그냥 ctrl + a 해서 실행하면 됩니다. 현재는 제작중 data.sql파일 맨 밑에 입력 프로시저 있습니다. 그거 활성화
+
+# 테이블 삭제
+DROP TABLE Selling;
+DROP TABLE Cart;
+DROP TABLE Orders;
+DROP TABLE Receiving;
+DROP TABLE Inventory;
+DROP TABLE Product;
+DROP TABLE Category;
+DROP TABLE Admin;
+
+
+# 테이블 생성
+CREATE TABLE Admin (
+                       admin_id            INT                     PRIMARY KEY AUTO_INCREMENT,
+                       password            VARCHAR(20)             NOT NULL
+)AUTO_INCREMENT = 1;
+
+CREATE TABLE Category (
+                          category_id         INT                     PRIMARY KEY AUTO_INCREMENT,
+                          name                VARCHAR(10)             NOT NULL,
+                          check_category      BOOLEAN                 NOT NULL DEFAULT TRUE,
+                          register_date       TIMESTAMP               NOT NULL DEFAULT CURRENT_TIMESTAMP
+)AUTO_INCREMENT = 1;
+
+CREATE TABLE Product (
+                         product_id          INT                     PRIMARY KEY AUTO_INCREMENT,
+                         category_id         INT                     NOT NULL,
+                         name                VARCHAR(20)             NOT NULL,
+                         code                VARCHAR(20)             NOT NULL,
+                         sell_price          INT                     NOT NULL,
+                         image               VARCHAR(30)             NOT NULL,
+                         check_product       BOOLEAN                 NOT NULL DEFAULT TRUE,
+                         register_date       TIMESTAMP               NOT NULL DEFAULT CURRENT_TIMESTAMP,
+                         CONSTRAINT Product_category_id_FK FOREIGN KEY (category_id) REFERENCES Category(category_id)
+)AUTO_INCREMENT = 1;
+
+CREATE TABLE Inventory (
+                           inventory_id            INT                 PRIMARY KEY AUTO_INCREMENT,
+                           product_id              INT                 NOT NULL,
+                           quantity                INT                 NOT NULL,
+                           register_date           TIMESTAMP           NOT NULL DEFAULT CURRENT_TIMESTAMP,
+                           CONSTRAINT Inventory_product_id_FK FOREIGN KEY (product_id) REFERENCES Product(product_id)
+)AUTO_INCREMENT = 1;
+
+CREATE TABLE Receiving (
+                           receiving_id            INT                 PRIMARY KEY AUTO_INCREMENT,
+                           product_id              INT                 NOT NULL,
+                           quantity                INT                 NOT NULL,
+                           description             VARCHAR(100)        NULL,
+                           check_receiving         BOOLEAN             NOT NULL DEFAULT TRUE,
+                           register_date           TIMESTAMP           NOT NULL DEFAULT CURRENT_TIMESTAMP,
+                           CONSTRAINT Receiving_product_id_FK FOREIGN KEY (product_id) REFERENCES Product(product_id)
+)AUTO_INCREMENT = 1000;
+
+CREATE TABLE Cart (
+                      Cart_id                 INT                 PRIMARY KEY AUTO_INCREMENT,
+                      product_id              INT                 NOT NULL,
+                      quantity                INT                 NOT NULL,
+                      total_price             INT                 NOT NULL,
+                      CONSTRAINT Cart_product_id_FK FOREIGN KEY (product_id) REFERENCES Product(product_id)
+)AUTO_INCREMENT = 1;
+
+CREATE TABLE Orders (
+                        orders_id               INT                 PRIMARY KEY AUTO_INCREMENT,
+                        check_orders            BOOLEAN             NOT NULL DEFAULT TRUE,
+                        register_date           TIMESTAMP           NOT NULL DEFAULT CURRENT_TIMESTAMP
+)AUTO_INCREMENT = 43102;
+
+CREATE TABLE Selling (
+                         selling_id              INT                 PRIMARY KEY AUTO_INCREMENT,
+                         product_id              INT                 NOT NULL,
+                         orders_id               INT                 NOT NULL,
+                         quantity                INT                 NOT NULL,
+                         total_price             INT                 NOT NULL,
+                         check_selling           BOOLEAN             NOT NULL DEFAULT TRUE,
+                         register_date           TIMESTAMP           NOT NULL DEFAULT CURRENT_TIMESTAMP,
+                         CONSTRAINT Selling_product_id_FK FOREIGN KEY (product_id) REFERENCES Product(product_id),
+                         CONSTRAINT Selling_orders_id_FK FOREIGN KEY (orders_id) REFERENCES Orders(orders_id)
+)AUTO_INCREMENT = 1;
+
+# 테이블 수정
+ALTER TABLE Cart ADD product_name VARCHAR(20) NOT NULL;
+ALTER TABLE Cart ADD image VARCHAR(20);
 
 # 카테고리 등록
 INSERT INTO Category (name)
@@ -405,33 +489,3 @@ CALL AddOrCountCart(2);
 CALL AddOrCountCart(2);
 CALL AddOrCountCart(2);
 call AddSellingAndClearCartTest(0);
-
-# 판매정보 날짜별 넣기
-drop procedure AddSellingAndClearCartTest;
-
-# 주문 완료 - 데이터 입력시 사용
-CREATE PROCEDURE AddSellingAndClearCartTest(IN num INT)
-
-BEGIN
-    INSERT INTO Orders (check_orders,register_date)
-    VALUES (TRUE,DATE_SUB(NOW(), INTERVAL num DAY));
-
-    SET @orders_id = NULL;
-    SET @orders_registerDate = NULL;
-
-    SELECT orders_id, register_date INTO @orders_id, @orders_registerDate
-    FROM Orders
-    ORDER BY orders_id DESC
-    LIMIT 1;
-
-    INSERT INTO Selling (product_id, orders_id, quantity, total_price, register_date)
-    SELECT product_id, @orders_id, quantity, total_price, @orders_registerDate FROM Cart;
-
-    UPDATE Inventory
-        JOIN Selling ON Inventory.product_id = Selling.product_id
-    SET Inventory.quantity = Inventory.quantity - Selling.quantity
-    WHERE Selling.orders_id = @orders_id;
-
-    TRUNCATE TABLE Cart;
-
-END;
