b17a1c4c5f5a4bdcdeae24fde51cf1f8230b1275
donoun6 <donoun6@naver.com>
2023-11-21 15:40:53 +0900

    수정

--- src/main/resources/static/db/initial.sql
+++ src/main/resources/static/db/initial.sql
@@ -2409,3 +2409,13 @@ CALL AddOrCountCart(FLOOR(1 + RAND() * 102));
 CALL AddOrCountCart(FLOOR(1 + RAND() * 102));
 call AddSellingAndClearCartTest(0);
 do sleep(1);
+
+CALL AddOrCountCart(3);
+CALL AddOrCountCart(3);
+CALL AddOrCountCart(3);
+CALL AddOrCountCart(3);
+CALL AddOrCountCart(3);
+CALL AddOrCountCart(3);
+CALL AddOrCountCart(3);
+call AddSellingAndClearCartTest(0);
+do sleep(1);
